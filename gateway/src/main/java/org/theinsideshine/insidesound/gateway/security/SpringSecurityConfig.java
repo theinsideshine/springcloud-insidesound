5a889ceb39df4b8e1a7a934be519e026090e39df
theinsideshine <educacion.ta@gmail.com>
2023-06-25 15:56:36 -0300

    microservices eureka-gateway-security-album work ok

---
+++ gateway/src/main/java/org/theinsideshine/insidesound/gateway/security/SpringSecurityConfig.java
@@ -0,0 +1,44 @@
+package org.theinsideshine.insidesound.gateway.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.theinsideshine.insidesound.gateway.security.filter.JwtAuthenticationFilter;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SpringSecurityConfig {
+
+    @Autowired
+    private JwtAuthenticationFilter authenticationFilter;
+
+    @Autowired
+    private CorsWebFilter corsWebFilter;
+
+    @Bean
+    public SecurityWebFilterChain configure(ServerHttpSecurity http) {
+        return http.authorizeExchange()
+                .pathMatchers("/msvc-security/login").permitAll()
+                .pathMatchers(HttpMethod.GET, "/msvc-security/users","/msvc-security/users/page/{page}").permitAll()
+                .pathMatchers(HttpMethod.GET, "/msvc-security/users/{id}").hasAnyRole("USER", "ADMIN")
+                .pathMatchers(HttpMethod.POST, "/msvc-security/users").permitAll()
+                .pathMatchers("/msvc-security/users/**").hasRole("ADMIN")
+                .pathMatchers( HttpMethod.GET,"/msvc-albums/**").permitAll()
+                .anyExchange().authenticated()
+                .and()
+                .addFilterAt(authenticationFilter, SecurityWebFiltersOrder.AUTHENTICATION)
+                .addFilterAt(corsWebFilter, SecurityWebFiltersOrder.CORS)
+                .csrf().disable()
+                .build();
+    }
+
+
+
+}
--- msvs-security/src/main/java/com/theinsideshine/backend/usersapp/usersapp/auth/SpringSecurityConfig.java
+++ msvs-security/src/main/java/com/theinsideshine/backend/usersapp/usersapp/auth/SpringSecurityConfig.java
@@ -1,13 +1,11 @@
 package com.theinsideshine.backend.usersapp.usersapp.auth;
 
-import java.util.Arrays;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.http.HttpMethod;
+
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -16,13 +14,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-
 import com.theinsideshine.backend.usersapp.usersapp.auth.filters.JwtAuthenticationFilter;
-import com.theinsideshine.backend.usersapp.usersapp.auth.filters.JwtValidationFilter;
+
 
 
 @Configuration
@@ -44,43 +37,14 @@ public class SpringSecurityConfig {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.authorizeHttpRequests()
-                .requestMatchers(HttpMethod.GET, "/users","/users/page/{page}").permitAll()
-                .requestMatchers(HttpMethod.GET, "/users/{id}").hasAnyRole("USER", "ADMIN")
-                .requestMatchers(HttpMethod.POST, "/users").permitAll()
-                .requestMatchers("/users/**").hasRole("ADMIN")
-                // .requestMatchers(HttpMethod.DELETE, "/users/{id}").hasRole("ADMIN")
-                // .requestMatchers(HttpMethod.PUT, "/users/{id}").hasRole("ADMIN")
-                .anyRequest().authenticated()
+
+                .anyRequest().permitAll()
                 .and()
                 .addFilter(new JwtAuthenticationFilter(authenticationConfiguration.getAuthenticationManager()))
-                .addFilter(new JwtValidationFilter(authenticationConfiguration.getAuthenticationManager()))
                 .csrf(config -> config.disable())
                 .sessionManagement(managment -> managment.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .build();
 
     }
 
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
-        config.setAllowedOriginPatterns(Arrays.asList("*"));
-        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
-        config.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
-        config.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-        return source;
-    }
-
-    @Bean
-    FilterRegistrationBean<CorsFilter> corsFilter() {
-        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(
-                new CorsFilter(corsConfigurationSource()));
-        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-        return bean;
-    }
 }
